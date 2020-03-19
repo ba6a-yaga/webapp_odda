2ecff265980884b876e587bc3b96906923ca5eb6
x3mprog <x3mprog@gmail.com>
2020-03-19 13:54:12 +0300

    Updates

---
+++ precache-manifest.5c138c9bc294fa791e264de39e9f9229.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "688717da7ab0e2d361bc2d9c911568e2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d3b45cbecba4021238ec",
+    "url": "./static/css/main.f908e47d.chunk.css"
+  },
+  {
+    "revision": "4e25a065c5f9da7334e8",
+    "url": "./static/js/2.15e268c1.chunk.js"
+  },
+  {
+    "revision": "d3b45cbecba4021238ec",
+    "url": "./static/js/main.ccb370e2.chunk.js"
+  },
+  {
+    "revision": "10277b50bc4dee08cfa2",
+    "url": "./static/js/runtime-main.1502ad92.js"
+  },
+  {
+    "revision": "2b136b429a0d1aa3782883baf378226f",
+    "url": "./static/media/Building.2b136b42.png"
+  },
+  {
+    "revision": "f81b5d8e5073cf4d3498b02c0cfab38c",
+    "url": "./static/media/Niz.f81b5d8e.png"
+  },
+  {
+    "revision": "7f5c21861a5e221f4bff78ca8f344900",
+    "url": "./static/media/NorseHeavy.7f5c2186.ttf"
+  },
+  {
+    "revision": "1a9dfcb93d757b385327a06ea815878f",
+    "url": "./static/media/Odal game trailer_1a.1a9dfcb9.mp4"
+  },
+  {
+    "revision": "b1564dd976b03c321cca4595527d5987",
+    "url": "./static/media/Pict_1.b1564dd9.png"
+  },
+  {
+    "revision": "da29e84cbcc5c7084d2adf6986944aca",
+    "url": "./static/media/Pict_2.da29e84c.png"
+  },
+  {
+    "revision": "5189cfa0330f52b0b5f03ddf3b2732d1",
+    "url": "./static/media/Screenshot_1.5189cfa0.png"
+  },
+  {
+    "revision": "2d1fda9279430d4a2d864a9c8559b4f2",
+    "url": "./static/media/Screenshot_2.2d1fda92.png"
+  },
+  {
+    "revision": "860bd6c85df8f9856b77c7a68923385f",
+    "url": "./static/media/Screenshot_3.860bd6c8.png"
+  },
+  {
+    "revision": "9d6f807ccf7f327c5c1ca9b822fecc94",
+    "url": "./static/media/Shapka_1.9d6f807c.png"
+  },
+  {
+    "revision": "6896f59be9a16f790dd813d53c8f5faa",
+    "url": "./static/media/fontello.6896f59b.eot"
+  },
+  {
+    "revision": "a425a277fcf195362af76525590a8a63",
+    "url": "./static/media/fontello.a425a277.woff"
+  },
+  {
+    "revision": "c05e845f6ee9b32758bd09c862b327a2",
+    "url": "./static/media/fontello.c05e845f.woff2"
+  },
+  {
+    "revision": "cdcbd5ef6a1fc2cc37dd48a8e1cda323",
+    "url": "./static/media/fontello.cdcbd5ef.svg"
+  },
+  {
+    "revision": "ee275448880c54f64d21c4793c987fee",
+    "url": "./static/media/fontello.ee275448.ttf"
+  },
+  {
+    "revision": "4352197a04fb0732f6780b13ba6cfe32",
+    "url": "./static/media/gplay.4352197a.png"
+  },
+  {
+    "revision": "351d137e2a840a416695b417f15c7c9f",
+    "url": "./static/media/itunes.351d137e.png"
+  },
+  {
+    "revision": "6733768f51f1cb629d708962c94ca07e",
+    "url": "./static/media/kanit.6733768f.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2b88bdfcfacc25ffab8449f5a2b5011d.js"
+  "./precache-manifest.5c138c9bc294fa791e264de39e9f9229.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
